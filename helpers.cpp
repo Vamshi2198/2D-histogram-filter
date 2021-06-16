a0b6fd579313187216a943a220f47494c17a1c98
Vamshi_Krishna <vamshikrishna.t21@gmail.com>
2021-06-15 21:43:54 -0400

    Implemented blur function

--- helpers.cpp
+++ helpers.cpp
@@ -42,3 +42,78 @@ vector< vector<float> > normalize(vector< vector <float> > grid) {
 
 	return newGrid;
 }
+
+/** Blurs (and normalizes) a grid of probabilities by spreading 
+    probability from each cell over a 3x3 "window" of cells. This 
+    function assumes a cyclic world where probability "spills 
+    over" from the right edge to the left and bottom to top. 
+
+    EXAMPLE - After blurring (with blurring=0.12) a localized 
+    distribution like this:
+
+    0.00  0.00  0.00 
+    0.00  1.00  0.00
+    0.00  0.00  0.00 
+
+    would look like this:
+	
+	0.01  0.02	0.01
+	0.02  0.88	0.02
+	0.01  0.02  0.01
+
+    @param grid - a two dimensional grid (vector of vectors of floats)
+		   where each entry represents the unnormalized probability 
+		   associated with that grid cell.
+
+	  @param blurring - a floating point number between 0.0 and 1.0 
+		   which represents how much probability from one cell 
+		   "spills over" to it's neighbors. If it's 0.0, then no
+		   blurring occurs. 
+
+    @return - a new normalized two dimensional grid where probability 
+    	   has been blurred.
+*/
+vector < vector <float> > blur(vector < vector < float> > grid, float blurring) {
+  
+  vector < vector <float> > newGrid(grid.size(), vector<float>(grid[0].size()));
+
+    int height = grid.size();
+    int width = grid[0].size();
+  
+ 
+    float center_prob = 1.0 - blurring;
+    float corner_prob = blurring/12.0;
+    float adjacent_prob = blurring/6.0;
+    
+    vector < vector <float> > window{ {corner_prob, adjacent_prob, corner_prob},
+                                      {adjacent_prob, center_prob, adjacent_prob},
+                                      {corner_prob, adjacent_prob, corner_prob}};
+    
+    for(int i =0; i<height; i++){
+     for(int j =0; j<width; j++){
+       new_grid[i][j] = 0*(grid[i][j]);
+
+      
+  
+  vector < vector <float> > grid_val;
+  int new_i;
+  int new_j;
+  for(int i =0; i<height; i++){
+     for(int j =0; j<width; j++){
+       grid_val[i][j] = grid[i][j];
+       for(int dx = -1; dx<window.size(); dx++){
+          for(int dy = -1; dy<window[0].size(); dy++){
+            new_i = (i + dy) % height;
+            new_j = (j + dx) % width;
+            newGrid[new_i][new_j] += window[dx+1][dy+1] * grid_val[i][j];
+          }
+       }            
+     }
+   }
+  
+   
+  
+    
+    
+	return normalize(newGrid);
+}
